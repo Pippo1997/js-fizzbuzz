539969b4acc7ae57a57be449e982eac079a90346
Filippo Sbaiz <sbaizfilippo@gmail.com>
2022-11-29 15:16:47 +0100

    Readme

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// PROBLEMA: Scrivi un programma che stampi in console i numeri da 1 a 100, ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”. Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”:
+
+// 1-Stampo in console dei numeri che vanno da 1 a 100
+//     1-1 Per i multipli di 3 stampo in console “Fizz”
+//     1.2 Per i multipli di 5 stampo in console “Buzz”
+//     1.3 Per i multipli sia di 3 che 5 stampo in console “FizzBuzz”
+// 2-Collego al DOM attraveso append()
+// 3-Stilizzo il DOm e mostro a video la soluzione “Fizz” “Buzz” “FizzBuzz”
